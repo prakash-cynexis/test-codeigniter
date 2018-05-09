f5b5b5c7555bb99f8ba7c0854b92e728b8037dd4
Prakash <prakash@cynexis.com>
2018-05-09 19:40:43 +0530

    add new function for check auth roles "authorizeRoles"

--- application/My_Classes/Auth/AuthorizesRequests.php
+++ application/My_Classes/Auth/AuthorizesRequests.php
@@ -43,4 +43,37 @@ trait AuthorizesRequests
 
         return $this->authUser;
     }
+
+    final public function authorizeRoles($roles = null)
+    {
+        $this->CI_GetInstance();
+
+        if (request()->isWeb()) {
+            if (!empty(getCurrentUserRole()) && !empty(getCurrentUser())) {
+                $this->authUser = true;
+
+                if ($roles) {
+                    if (is_string($roles)) $roles = [$roles];
+                    $this->authUser = in_array(getCurrentUserRole(), $roles);
+                }
+            }
+        }
+
+        // The code for all app request are authenticate.
+        if (request()->isApp()) {
+            if (!in_array($this->CI->router->method, $this->CI->_skip_auth_methods)) {
+                $this->CI->authToken = is_authenticated();
+                $this->authUser = true;
+
+                if ($roles) {
+                    if (is_string($roles)) $roles = [$roles];
+                    $this->authUser = in_array($this->CI->authToken['role'], $roles);
+                }
+            } else {
+                $this->authUser = true;
+            }
+        }
+
+        return $this->authUser;
+    }
 }
