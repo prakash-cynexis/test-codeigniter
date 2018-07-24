66aae292bc3bde1d3e13cef07b8dbb13713586c0
Prakash <prakash@cynexis.com>
2018-07-24 17:57:27 +0530

    1) some changes in CLI command line function.

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,7 +3,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class User_model extends MY_Model
 {
-    protected $protectedColumn = ['user_name', 'email', 'password', 'role', 'is_active', 'auth_token', 'deleted'];
+    protected $protectedColumn = ['user_name', 'email', 'password', 'role_id', 'is_active', 'auth_token', 'deleted'];
     protected $before_create = ['removeUnknownColumn', 'hashPassword', 'revertViewToTable'];
     protected $before_update = ['removeUnknownColumn', 'hashPassword', 'revertViewToTable'];
 
