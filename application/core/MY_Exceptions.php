45686f05561e39244ffdba31e772030166a9f090
Prakash <prakash@cynexis.com>
2018-09-28 17:10:12 +0530

    1) make new function to check last uri string.

--- application/core/MY_Exceptions.php
+++ application/core/MY_Exceptions.php
@@ -5,7 +5,7 @@ class MY_Exceptions extends CI_Exceptions
     function show_error($heading, $message, $template = 'error_general', $status_code = 500)
     {
         log_message('error', print_r($message, TRUE));
-        //throw new Exception(is_array($message) ? $message[1] : $message, $status_code );
+        if (!is_array($message)) $message = [$message];
         header('Content-Type: application/json', true, $status_code);
         exit(json_encode(array(
             'error' => true,
